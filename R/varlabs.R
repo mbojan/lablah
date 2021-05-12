f45baf0b9e048820207792b0cf6ccaecc6c1b96e
Michal Bojanowski <michal2992@gmail.com>
2021-05-12 23:19:30 +0200

    Add varlabs()

---
+++ R/varlabs.R
@@ -0,0 +1,16 @@
+#' Data frame of variable labels
+#'
+#' @param data data frame with labelled variables
+#'
+#' @return A tibble with columns
+#' - `variable` - Variable name
+#' - `label` - Variable label
+#'
+#' @seealso [labelled::var_label()]
+#'
+#' @export
+
+varlabs <- function(data) {
+  varlabs <- labelled::var_label(data)
+  tibble::enframe(unlist(varlabs), name = "variable", value = "label")
+}
